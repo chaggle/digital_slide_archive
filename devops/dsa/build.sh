362c5c2802ceec272f31bf8524cf9f867253404a
chaggle <chaggle@foxmail.com>
2025-04-07 17:21:46 +0800

    chore(build.sh): 添加清理未使用镜像的步骤

--- devops/dsa/build.sh
+++ devops/dsa/build.sh
@@ -1,2 +1,4 @@
 docker pull harbor.5gweilaixilie.cn:8443/annation/dsa:latest
 DSA_USER=$(id -u):$(id -g) docker compose up -d
+image = $(docker images | grep none | awk '{print $3}')
+docker rmi $image
